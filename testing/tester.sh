16323eaaa216028273a770ded18bc7decd9e2195
Markus Dittrich <haskelladdict@gmail.com>
2014-02-15 11:55:55 -0500

    Updated tester.
    Added ability to test only selected files.

--- testing/tester.sh
+++ testing/tester.sh
@@ -3,7 +3,12 @@
 # this shell scripts runs go test on all go-gsl test cases
 # (C) 2014 Markus Dittrich <haskelladdict@gmail.com>
 
-packages=$(find ../ -name "*test.go" | xargs dirname)
+
+if [[ $1 != "" ]]; then
+  packages=$1
+else
+  packages=$(find ../ -name "*test.go" | xargs dirname)
+fi
 
 for package in ${packages[*]}; do
   go test ${package}
