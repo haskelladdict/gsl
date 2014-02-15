2827a7b6a3f63bb20da359d713c6a558ae99afc3
Markus Dittrich <haskelladdict@gmail.com>
2014-02-15 13:24:26 -0500

    Added additional stats functions and tests.

--- src/stats/stats.go
+++ src/stats/stats.go
@@ -17,65 +17,65 @@ import "C"
 type FloatSlice []float64
 
 // Mean returns the arithmetic mean of data with stride stride.
-func (f FloatSlice) Mean(stride int) float64 {
-  mean := C.gsl_stats_mean((*C.double)(&f[0]), C.size_t(stride),
-    C.size_t(len(f)))
+func (d FloatSlice) Mean(stride int) float64 {
+  mean := C.gsl_stats_mean((*C.double)(&d[0]), C.size_t(stride),
+    C.size_t(len(d)))
   return float64(mean)
 }
 
 // Variance returns the variance of data with stride stride.
-func (f FloatSlice) Variance(stride int) float64 {
-  variance := C.gsl_stats_variance((*C.double)(&f[0]), C.size_t(stride),
-    C.size_t(len(f)))
+func (d FloatSlice) Variance(stride int) float64 {
+  variance := C.gsl_stats_variance((*C.double)(&d[0]), C.size_t(stride),
+    C.size_t(len(d)))
   return float64(variance)
 }
 
 // Variance_m returns the variance of data with stride stride and a user
 // supplied value for the mean
-func (f FloatSlice) Variance_m(stride int, mean float64) float64 {
-  variance_m := C.gsl_stats_variance_m((*C.double)(&f[0]), C.size_t(stride),
-    C.size_t(len(f)), C.double(mean))
+func (d FloatSlice) Variance_m(stride int, mean float64) float64 {
+  variance_m := C.gsl_stats_variance_m((*C.double)(&d[0]), C.size_t(stride),
+    C.size_t(len(d)), C.double(mean))
   return float64(variance_m)
 }
 
 // Sd returns the standard deviation of data with stride stride.
 // NOTE: The value of this function equals the square root of the variance
-func (f FloatSlice) Sd(stride int) float64 {
-  sd := C.gsl_stats_sd((*C.double)(&f[0]), C.size_t(stride), C.size_t(len(f)))
+func (d FloatSlice) Sd(stride int) float64 {
+  sd := C.gsl_stats_sd((*C.double)(&d[0]), C.size_t(stride), C.size_t(len(d)))
   return float64(sd)
 }
 
 // Sd_m returns the standard deviation of data with stride stride and a user
 // supplied value for the mean.
 // NOTE: The value of this function equals the square root of the variance
-func (f FloatSlice) Sd_m(stride int, mean float64) float64 {
-  sd_m := C.gsl_stats_sd_m((*C.double)(&f[0]), C.size_t(stride),
-    C.size_t(len(f)), C.double(mean))
+func (d FloatSlice) Sd_m(stride int, mean float64) float64 {
+  sd_m := C.gsl_stats_sd_m((*C.double)(&d[0]), C.size_t(stride),
+    C.size_t(len(d)), C.double(mean))
   return float64(sd_m)
 }
 
 // Tss returns the total sum of squares (TSS) of data about the mean with
 // stride stride.
-func (f FloatSlice) Tss(stride int) float64 {
-  tss := C.gsl_stats_tss((*C.double)(&f[0]), C.size_t(stride),
-    C.size_t(len(f)))
+func (d FloatSlice) Tss(stride int) float64 {
+  tss := C.gsl_stats_tss((*C.double)(&d[0]), C.size_t(stride),
+    C.size_t(len(d)))
   return float64(tss)
 }
 
 // Tss_m returns the total sum of squares (TSS) of data about the mean with
 // stride stride and a user supplied value for the mean.
-func (f FloatSlice) Tss_m(stride int, mean float64) float64 {
-  tss_m := C.gsl_stats_tss_m((*C.double)(&f[0]), C.size_t(stride),
-    C.size_t(len(f)), C.double(mean))
+func (d FloatSlice) Tss_m(stride int, mean float64) float64 {
+  tss_m := C.gsl_stats_tss_m((*C.double)(&d[0]), C.size_t(stride),
+    C.size_t(len(d)), C.double(mean))
   return float64(tss_m)
 }
 
 // Variance_with_fixed_mean computes an unbiased estimate of the variance
 // of data when the population mean mean of the underlying distribution is
 // known a priori.
-func (f FloatSlice) Variance_with_fixed_mean(stride int, mean float64) float64 {
-  var_fixed := C.gsl_stats_variance_with_fixed_mean((*C.double)(&f[0]),
-    C.size_t(stride), C.size_t(len(f)), C.double(mean))
+func (d FloatSlice) Variance_with_fixed_mean(stride int, mean float64) float64 {
+  var_fixed := C.gsl_stats_variance_with_fixed_mean((*C.double)(&d[0]),
+    C.size_t(stride), C.size_t(len(d)), C.double(mean))
   return float64(var_fixed)
 }
 
@@ -83,28 +83,43 @@ func (f FloatSlice) Variance_with_fixed_mean(stride int, mean float64) float64 {
 // of data when the population mean mean of the underlying distribution is
 // known a priori.
 // NOTE: This is equal to the square root of the Variance_with_fixed_mean
-func (f FloatSlice) Sd_with_fixed_mean(stride int, mean float64) float64 {
-  var_fixed := C.gsl_stats_sd_with_fixed_mean((*C.double)(&f[0]),
-    C.size_t(stride), C.size_t(len(f)), C.double(mean))
+func (d FloatSlice) Sd_with_fixed_mean(stride int, mean float64) float64 {
+  var_fixed := C.gsl_stats_sd_with_fixed_mean((*C.double)(&d[0]),
+    C.size_t(stride), C.size_t(len(d)), C.double(mean))
   return float64(var_fixed)
 }
 
 
 // Absdev computes the absolute deviation from the mean of data with 
 // stride stride
-func (f FloatSlice) Absdev(stride int) float64 {
-  absdev := C.gsl_stats_absdev((*C.double)(&f[0]), C.size_t(stride),
-    C.size_t(len(f)))
+func (d FloatSlice) Absdev(stride int) float64 {
+  absdev := C.gsl_stats_absdev((*C.double)(&d[0]), C.size_t(stride),
+    C.size_t(len(d)))
   return float64(absdev)
 }
 
 
 // Absdev_m computes the absolute deviation from the mean of data with 
 // stride stride and given mean
-func (f FloatSlice) Absdev_m(stride int, mean float64) float64 {
-  absdev_m := C.gsl_stats_absdev_m((*C.double)(&f[0]), C.size_t(stride),
-    C.size_t(len(f)), C.double(mean))
+func (d FloatSlice) Absdev_m(stride int, mean float64) float64 {
+  absdev_m := C.gsl_stats_absdev_m((*C.double)(&d[0]), C.size_t(stride),
+    C.size_t(len(d)), C.double(mean))
   return float64(absdev_m)
 }
 
 
+// Skew computes the skewness of data with stride stride.
+func (d FloatSlice) Skew(stride int) float64 {
+  skew := C.gsl_stats_skew((*C.double)(&d[0]), C.size_t(stride),
+    C.size_t(len(d)))
+  return float64(skew)
+}
+
+
+// Skew_m_sd computes the skewness of the dataset data using stride stride
+// and the given values of the mean mean and standard deviation sd.
+func (d FloatSlice) Skew_m_sd(stride int, mean float64, sd float64) float64 {
+  skew_m_sd := C.gsl_stats_skew_m_sd((*C.double)(&d[0]), C.size_t(stride),
+    C.size_t(len(d)), C.double(mean), C.double(sd))
+  return float64(skew_m_sd)
+}
--- src/stats/stats_test.go
+++ src/stats/stats_test.go
@@ -15,33 +15,33 @@ import (
   "testing"
 )
 
-func Test_stats(t *testing.T) {
+func Test_stats_1(t *testing.T) {
 
   data := FloatSlice{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0}
 
   mean := data.Mean(1)
   if !util.FloatEqual(mean, 5.5) {
-    t.Error("Failed to compute mean")
+    t.Error("Test 1: Failed to compute mean")
   }
 
   sigma2 := data.Variance(1)
   if !util.FloatEqual(sigma2, 9.166666666666666) {
-    t.Error("Failed to compute variance.")
+    t.Error("Test 1: Failed to compute variance.")
   }
 
   sigma2_m := data.Variance_m(1, mean)
   if !util.FloatEqual(sigma2_m, 9.166666666666666) {
-    t.Error("Failed to compute variance.")
+    t.Error("Test 1: Failed to compute variance.")
   }
 
   sigma := data.Sd(1)
   if !util.FloatEqual(sigma, math.Sqrt(sigma2)) {
-    t.Error("Failed to compute standard deviation.")
+    t.Error("Test 1: Failed to compute standard deviation.")
   }
 
   sigma_m := data.Sd_m(1, mean)
   if !util.FloatEqual(sigma_m, math.Sqrt(sigma2)) {
-    t.Error("Failed to compute standard deviation with mean.")
+    t.Error("Test 1: Failed to compute standard deviation with mean.")
   }
 
   tss_comp := 0.0
@@ -51,31 +51,69 @@ func Test_stats(t *testing.T) {
 
   tss := data.Tss(1)
   if !util.FloatEqual(tss, tss_comp) {
-    t.Error("Failed to compute total sum of squares.")
+    t.Error("Test 1: Failed to compute total sum of squares.")
   }
 
   tss_m := data.Tss_m(1, mean)
   if !util.FloatEqual(tss, tss_m) {
-    t.Error("Failed to compute total sum of squares with mean.")
+    t.Error("Test 1: Failed to compute total sum of squares with mean.")
   }
 
   sigma2_fixed := data.Variance_with_fixed_mean(1, mean)
   if !util.FloatEqual(sigma2_fixed, 8.25) {
-    t.Error("Failed to compute variance with fixed mean.")
+    t.Error("Test 1: Failed to compute variance with fixed mean.")
   }
 
   sigma_fixed := data.Sd_with_fixed_mean(1, mean)
   if !util.FloatEqual(sigma_fixed, math.Sqrt(sigma2_fixed)) {
-    t.Error("Failed to compute standard deviation with fixed mean.")
+    t.Error("Test 1: Failed to compute standard deviation with fixed mean.")
   }
 
   absdev := data.Absdev(1)
   if !util.FloatEqual(absdev, 2.5) {
-    t.Error("Failed to compute absolute deviation.")
+    t.Error("Test 1: Failed to compute absolute deviation.")
   }
 
   absdev_m := data.Absdev_m(1, mean)
   if !util.FloatEqual(absdev_m, absdev) {
-    t.Error("Failed to compute absolute deviation with mean.")
+    t.Error("Test 1: Failed to compute absolute deviation with mean.")
+  }
+}
+
+
+
+func Test_stats_2(t *testing.T) {
+
+  norm_data := FloatSlice{1.33830225764885e-04, 3.52595682367445e-04,
+    8.72682695045760e-04, 2.02904805729977e-03, 4.43184841193801e-03,
+    9.09356250159105e-03, 1.75283004935685e-02, 3.17396518356674e-02,
+    5.39909665131881e-02, 8.62773188265115e-02, 1.29517595665892e-01,
+    1.82649085389022e-01, 2.41970724519143e-01, 3.01137432154804e-01,
+    3.52065326764300e-01, 3.86668116802849e-01, 3.98942280401433e-01,
+    3.86668116802849e-01, 3.52065326764300e-01, 3.01137432154804e-01,
+    2.41970724519143e-01, 1.82649085389022e-01, 1.29517595665892e-01,
+    8.62773188265115e-02, 5.39909665131881e-02, 3.17396518356674e-02,
+    1.75283004935685e-02, 9.09356250159105e-03, 4.43184841193801e-03,
+    2.02904805729977e-03, 8.72682695045760e-04, 3.52595682367445e-04,
+    1.33830225764885e-04}
+
+  mean := norm_data.Mean(1)
+  if !util.FloatEqual(mean, 0.12120783192361629654) {
+    t.Error("Test 2: Failed to compute mean.")
+  }
+
+  sd := norm_data.Sd(1)
+  if !util.FloatEqual(sd, 0.1418146888138111239) {
+    t.Error("Test 2: Failed to compute standard deviation.")
+  }
+
+  skew := norm_data.Skew(1)
+  if !util.FloatEqual(skew, 0.79446149715833858096) {
+    t.Error("Test 2: Failed to compute skew.")
+  }
+
+  skew_m_sd := norm_data.Skew_m_sd(1, mean, sd)
+  if !util.FloatEqual(skew_m_sd, skew) {
+    t.Error("Test 2: Failed to compute skew with mean and stddev.")
   }
 }
