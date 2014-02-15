f8ef63a8272a21b3e0bac70d332ca060ca050564
Markus Dittrich <haskelladdict@gmail.com>
2014-02-15 11:53:42 -0500

    Added more standard deviation tests.

--- src/stats/stats_test.go
+++ src/stats/stats_test.go
@@ -11,6 +11,7 @@ package stats
 
 import (
   "github.com/haskelladdict/go-gsl/util"
+  "math"
   "testing"
 )
 
@@ -23,14 +24,48 @@ func Test_stats(t *testing.T) {
     t.Error("Failed to compute mean")
   }
 
-  sigma := data.Variance(1)
-  if !util.FloatEqual(sigma, 9.166666666666666) {
-    t.Error("Failed to compute variance.", sigma)
+  sigma2 := data.Variance(1)
+  if !util.FloatEqual(sigma2, 9.166666666666666) {
+    t.Error("Failed to compute variance.")
   }
 
-  sigma_m := data.Variance_m(1, mean)
-  if !util.FloatEqual(sigma_m, 9.166666666666666) {
-    t.Error("Failed to compute variance.", sigma)
+  sigma2_m := data.Variance_m(1, mean)
+  if !util.FloatEqual(sigma2_m, 9.166666666666666) {
+    t.Error("Failed to compute variance.")
   }
 
+  sigma := data.Sd(1)
+  if !util.FloatEqual(sigma, math.Sqrt(sigma2)) {
+    t.Error("Failed to compute standard deviation.")
+  }
+
+  sigma_m := data.Sd_m(1, mean)
+  if !util.FloatEqual(sigma_m, math.Sqrt(sigma2)) {
+    t.Error("Failed to compute standard deviation with mean.")
+  }
+
+  tss_comp := 0.0
+  for _, v := range data {
+    tss_comp += (v - mean) * (v - mean)
+  }
+
+  tss := data.Tss(1)
+  if !util.FloatEqual(tss, tss_comp) {
+    t.Error("Failed to compute total sum of squares.")
+  }
+
+  tss_m := data.Tss_m(1, mean)
+  if !util.FloatEqual(tss, tss_m) {
+    t.Error("Failed to compute total sum of squares with mean.")
+  }
+
+  sigma2_fixed := data.Variance_with_fixed_mean(1, mean)
+  if !util.FloatEqual(sigma2_fixed, 8.25) {
+    t.Error("Failed to compute variance with fixed mean.")
+  }
+
+  sigma_fixed := data.Sd_with_fixed_mean(1, mean)
+  if !util.FloatEqual(sigma_fixed, math.Sqrt(sigma2_fixed)) {
+    t.Error("Failed to compute standard deviation with fixed mean.")
+  }
 }
