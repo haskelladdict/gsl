b74e02a8b68c943d9da57b8ece85bc43712d6186
Markus Dittrich <haskelladdict@gmail.com>
2014-02-22 12:18:55 -0500

    Implemented additional random number functions.

--- testing/runner.go
+++ testing/runner.go
@@ -17,10 +17,15 @@ func main() {
   fmt.Println("mean is ", data.Mean(1), data.Variance(1),
     data.Variance_with_fixed_mean(1, 3.0), data.Absdev(1))
 */
-  rng_type := random.Default()
-  rng_state := random.Alloc(rng_type)
-  fmt.Println(rng_state.Name())
-  for i:=0; i < 100; i++ {
-    fmt.Println(rng_state.Uniform())
+  //rng_type := random.Default()
+  rng_state := random.Alloc(random.Mt19937)
+  rng_state.Set(3241)
+  for i:=0; i < 10; i++ {
+    fmt.Println(rng_state.UniformInt(100))
   }
+
+  fmt.Println(rng_state.Name())
+  fmt.Println(rng_state)
+  fmt.Println(rng_state.Max(), rng_state.Min())
+  fmt.Println(rng_state.State(), rng_state.Size())
 }
